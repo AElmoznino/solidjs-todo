89434cb8efb19f363d4866ab5ad31711c94b4c1e
André Elmoznino Laufer <andre.elmoznino@valtech.com>
2021-05-20 13:50:29 +0200

    Init 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Todo from './components/Todo'
 
-function App() {
+const App = () => {
   return (
     <div class="App">
-      <header class="App-header">
-        <img src={logo} class="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class="App-link"
-          href="https://github.com/ryansolid/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+     <header>Some header</header>
+     <Todo />
+     <footer>Some footer</footer>
     </div>
   );
 }
