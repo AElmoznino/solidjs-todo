808c8a8d85923c1d272b637d9f7cd42a89e46b6e
davidwajngot <david.wajngot@valtech.com>
2021-05-20 14:08:10 +0200

    init Todo component

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,4 +1,26 @@
+import { createState } from "solid-js";
+
+export enum Status {
+  Started = "started",
+  Completed = "completed",
+  Active = "active"
+}
+
 const Todo = () => {
+  const [state, setState] = createState({
+    todos: [
+      {
+        id: 1,
+        title: "Garden Work",
+        status: Status.Active
+      },
+      {
+        id: 2,
+        title: "Work out",
+        status: Status.Completed
+      },
+    ]
+  })
 
   return (
     <div class="App">
